3807266c2b4e8df349e080a7831a7d3dd1fc3183
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-10-27 17:09:08 +0200

    final commit

--- datasets/download_partial_data.sh
+++ datasets/download_partial_data.sh
@@ -4,6 +4,8 @@ wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 # VQA Input Questions
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip
+# VQA Input Images
+# modified version of the images datasets --> for efficency reasons
 gdown https://drive.google.com/uc?id=15oPLN-jZvlIjvhvPJNrNk-SFvInsqZsC
 # id alternative: 1hbmQJ7jwjMRgSNaKU-X1DPHJKwX4IDHu
 unzip -q \*.zip
